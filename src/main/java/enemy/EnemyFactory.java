e1d8859672fe2588799b91fddf35bd7afbf03fd4
lukasz <slaskww@gmail.com>
2019-05-12 15:57:29 +0200

    Add method creating a boss in class  EnenmyFactory

--- src/main/java/enemy/EnemyFactory.java
+++ src/main/java/enemy/EnemyFactory.java
@@ -1,4 +1,13 @@
 package enemy;
 
+import weapon.Weapon;
+
 public class EnemyFactory {
+
+public static Enemy boss(){
+    Enemy enemy = new Enemy("Officer Barbrady", 40, 40,  new Weapon());
+    return enemy;
+}
+
+
 }
